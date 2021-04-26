add4fb019ebb6dc52699fc5a0f77cda207631c4c
Jhalak Maheshwari <maheshwari.jhalak@gmail.com>
2021-04-26 00:08:37 -0700

    own bind function creating own bind function with prototype

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import "./styles.css";
 
+let name = {
+  fName: "Jhalak",
+  lName: "Maheshwari"
+};
+
+let printName = function (hometown, state) {
+  console.log(
+    "firstname::",
+    this.fName,
+    "lastname::",
+    this.lName,
+    hometown,
+    state
+  );
+};
+
+let printMyName = printName.bind(name, "Jaipur", "Rajasthan");
+printMyName("Rajasthan");
+
+// Make your own Bind function
+Function.prototype.myBind = function (...args) {
+  let obj = this;
+  let params = args.slice(1);
+  return function (...args2) {
+    obj.apply(args[0], [...params, ...args2]);
+  };
+};
+
+let printMyName2 = printName.myBind(name, "Jaipur");
+printMyName2("Rajasthan");
+
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
